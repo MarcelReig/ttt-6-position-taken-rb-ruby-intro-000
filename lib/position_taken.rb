939e9c1d7bd204d036320edfc8399cf192f25bf9
Marcel Reig <marcelreig@gmail.com>
2019-06-23 10:35:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
-    false
+    return false
     elsif board[index] == "X" || "O"
-    true
+    return true
   end
 end
